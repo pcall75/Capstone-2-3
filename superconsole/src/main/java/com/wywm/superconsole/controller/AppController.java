b32ba939ee4f74f68c3f99cd5f0153e4a041d3c8
pcall75 <patrickdcallaghan@gmail.com>
2022-07-24 23:00:18 +0100

    Commit

--- superconsole/src/main/java/com/wywm/superconsole/controller/AppController.java
+++ superconsole/src/main/java/com/wywm/superconsole/controller/AppController.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import java.util.List;
-
 
 @Controller
 public class AppController {
