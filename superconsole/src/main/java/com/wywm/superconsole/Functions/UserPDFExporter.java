0d8ad221abb9f325beebe1f38a87ef79495f3cdd
NeetKB <akbhandal@gmail.com>
2022-07-27 11:02:53 +0100

    Integrated the functions2

---
+++ superconsole/src/main/java/com/wywm/superconsole/Functions/UserPDFExporter.java
@@ -0,0 +1,75 @@
+package com.wywm.superconsole.Functions;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class UserPDFExporter {
+
+    private List<Troops> listTroops;
+
+
+
+
+    public  UserPDFExporter(List<Troops> listTroops) {
+         this.listTroops = listTroops;
+    }
+
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(BaseColor.WHITE);
+        cell.setPadding(2);
+
+        com.itextpdf.text.Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(BaseColor.BLACK);
+
+        cell.setPhrase(new Phrase("User ID", font));
+
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Full Name", font));
+        table.addCell(cell);
+
+    }
+
+    private void writeTableData(PdfPTable table) {
+        for ( Troops Troops : listTroops) {
+            table.addCell(String.valueOf(Troops.getId()));
+            table.addCell(Troops.getFullName());
+        }
+    }
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(18);
+        font.setColor(BaseColor.WHITE);
+
+        Paragraph p = new Paragraph("List of Soldiers Deployed", font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+
+        document.add(p);
+
+        PdfPTable table = new PdfPTable(2);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[] {3.0f, 3.0f});
+        table.setSpacingBefore(10);
+
+        writeTableHeader(table);
+        writeTableData(table);
+
+        document.add(table);
+
+        document.close();
+
+    }
+
+}
---
+++ superconsole/src/main/java/com/wywm/superconsole/user/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.wywm.superconsole.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<User, Integer> {
+
+
+}
