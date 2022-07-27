a5cc5e318b22ea44f7116df3917f399d7fb03dfa
pcall75 <patrickdcallaghan@gmail.com>
2022-07-27 10:12:32 +0100

    Commit

--- superconsole/src/main/java/com/wywm/superconsole/user/TroopUserDetails.java
+++ superconsole/src/main/java/com/wywm/superconsole/user/TroopUserDetails.java
@@ -1,9 +1,14 @@
 package com.wywm.superconsole.user;
 
+import com.wywm.superconsole.security.Role;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 public class TroopUserDetails implements UserDetails {
 
@@ -15,7 +20,14 @@ public class TroopUserDetails implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		Set<Role> roles = user.getRoles();
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+		for (Role role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role.getName()));
+		}
+
+		return authorities;
 	}
 
 	@Override
